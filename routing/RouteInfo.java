c4c14c781c1efb062cb2499e63985112ac0c5f6b
Nicky Setia <nicky.setia@gmail.com>
2014-05-30 11:25:11 -0400

    Create RouteInfo.java

---
+++ routing/RouteInfo.java
@@ -0,0 +1,16 @@
+/**
+ * Information on a route returned by an instance of RoutingService.
+ */
+public class RouteInfo {
+	private List<LatLong> waypoints;
+
+	public RouteInfo(List<LatLong> waypoints) {
+		this.waypoints = waypoints;
+	}
+
+	public List<LatLong> getWaypoints() {
+		return waypoints;
+	}
+	
+	
+}
